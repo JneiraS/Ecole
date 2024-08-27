d8065fdd31d47c0ae4dd15efc22aab41a5696931
NeiraJ <johnny.neira@gmail.com>
2024-08-27 12:55:27 +0200

    - Création de la classe PersonCreator dans factories.py pour gérer la création de personnes à partir des résultats des requêtes.

--- src/factories.py
+++ src/factories.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+from DAO.person_dao import PersonManager
+from models.person import Person
+from src.table_name import TableName
 from DAO.course_dao import CourseManager
 from models.course import Course
 
@@ -11,20 +14,32 @@ class Creator(ABC):
 
 
 class CourseCreator(Creator):
-    def factory_method(self, information_source):
+    def factory_method(self, information_source: dict):
         Course.create_course(information_source['name'], information_source['start_date'],
                              information_source['end_date'])
         Course.id = information_source['id_course']
 
+
+class PersonCreator(Creator):
+    def factory_method(self, information_source: dict):
+        Person.create_person(information_source['first_name'], information_source['last_name'],
+                             information_source['age'], information_source['id_address'])
+        Person.id = information_source['id_person']
 
 def create_courses_from_query_results():
     """
     Crée des objets Course à partir de la base de données
     """
     course = CourseManager()
-    results: list[dict] = course.query_all('course')
+    results: list[dict] = course.query_all(TableName.COURSE.value)
     list(map(CourseCreator().factory_method, results))
 
-
+def create_persons_from_query_results():
+    """
+    Crée des objets Person à partir de la base de données
+    """
+    person = PersonManager()
+    results: list[dict] = person.query_all(TableName.PERSON.value)
+    list(map(PersonCreator().factory_method, results))
 
 
