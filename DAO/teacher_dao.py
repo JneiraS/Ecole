496f46ebb6d6a2c1530588da02122b2da8f07d38
NeiraJ <johnny.neira@gmail.com>
2024-08-27 17:42:20 +0200

    feature:  methode creation : Crée un nouvel enseignant dans la base de données.

--- DAO/teacher_dao.py
+++ DAO/teacher_dao.py
@@ -9,10 +9,11 @@ class TeacherManager(DatabaseConnectionManager):
         try:
             self.open_connection()
 
-            query = (f"SELECT t.id_teacher,t.start_date, p.first_name, p.last_name, p.age, p.id_address, t.id_person "
-                     f"FROM {table_name} t "
-                     f"INNER JOIN "
-                     f"person p ON t.id_person = p.id_person;")
+            query = (
+                f"SELECT t.id_teacher,t.start_date, p.first_name, p.last_name, p.age, p.id_address, t.id_person "
+                f"FROM {table_name} t "
+                f"INNER JOIN "
+                f"person p ON t.id_person = p.id_person;")
             self.cursor.execute(query)
 
             rows = self.cursor.fetchall()
@@ -27,18 +28,25 @@ class TeacherManager(DatabaseConnectionManager):
 
     def create(self, new_teacher: Teacher):
         """
-        Crée un nouveau cours dans la base de données.
+        Crée un nouvel enseignant dans la base de données.
+        ""
         :param new_teacher:
         :return:
         """
         try:
             self.open_connection()
-            query = (f"START TRANSACTION;"
-                     f"INSERT INTO person (first_name, last_name, age, id_address)"
-                     f"VALUES ('John', 'Doe', 30, 1);"
-                     f"INSERT INTO teacher (start_date, id_person)"
-                     f"VALUES ('2024-08-27', LAST_INSERT_ID());"
-                     f"COMMIT;")
+
+            query = f"START TRANSACTION;"
+            self.cursor.execute(query)
+            # Créer la personne
+            query = (f"INSERT INTO person (first_name, last_name, age) "
+                     f"VALUES ('{new_teacher.first_name}', '{new_teacher.last_name}', {new_teacher.age});")
+            print(query)
+            self.cursor.execute(query)
+            # Créer l'enseignant en récupérant l'ID de la personne créée
+            query = (f"INSERT INTO teacher (start_date, id_person)"
+                     f"VALUES ('{new_teacher.hiring_date}', "
+                     f"LAST_INSERT_ID());")
 
             self.cursor.execute(query)
             self.conn.commit()
@@ -73,13 +81,24 @@ class TeacherManager(DatabaseConnectionManager):
 
         return None
 
-    def update(self, course_to_update: Teacher):
-        """Mett à jour la base de données avec les nouvelles informations du cours."""
+    def update(self, teacher_to_update: Teacher):
+        """Mett à jour la base de données avec les nouvelles informations du professeur."""
         self.open_connection()
-        query = (f" UPDATE course SET name = '{course_to_update.name}', start_date = '"
-                 f"{course_to_update.start_date}', end_date = '{course_to_update.end_date}', id_teacher = "
-                 f"{course_to_update.teacher};")
+
+        query = f"SELECT id_person FROM teacher WHERE id_teacher = '{teacher_to_update.id}';"
         self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        id_person = result['id_person']
+        print(teacher_to_update.age)
+
+        query = (f"UPDATE person SET "
+                 f"first_name = '{teacher_to_update.first_name}', "
+                 f"last_name = '{teacher_to_update.last_name}', "
+                 f"age = {teacher_to_update.age} "
+                 f"WHERE id_person = '{id_person}';")
+
+        self.cursor.execute(query)
+        self.conn.commit()
         self.close_connection()
 
     def delete(self):
