a08cb70484c4ee0000cfcf30434cac06798ca7be
NeiraJ <johnny.neira@gmail.com>
2024-08-27 12:56:36 +0200

    Introduction de méthodes de création dans Person et Course pour faciliter la gestion dynamique des instances.

--- models/person.py
+++ models/person.py
@@ -12,6 +12,10 @@ from .address import Address
 @dataclass
 class Person(ABC):
     """Personne liée à l'école : enseignant ou élève."""
+
+    person_list = []
+
+    id: int = field(default=None, init=False)
     first_name: str
     last_name: str
     age: int
@@ -19,3 +23,18 @@ class Person(ABC):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} ({self.age} ans)"
+
+    @classmethod
+    def create_person(cls, first_name, last_name, age, address):
+        """
+        Crée un nouvel objet Person et l'ajoute à la liste des personnes.
+        :param first_name:
+        :param last_name:
+        :param age:
+        :param address:
+        :return:
+        """
+        new_person = cls(first_name=first_name, last_name=last_name, age=age)
+        Person.person_list.append(new_person)
+        return new_person
+
