1d72a163c2b03bffd10a1ae968b998e1961ae825
NeiraJ <johnny.neira@gmail.com>
2024-08-27 13:37:54 +0200

    Introduction de méthodes de création dans Adress

--- models/adress.py
+++ models/adress.py
@@ -11,7 +11,16 @@ from typing import Optional
 @dataclass
 class Adress:
     """Adresse d'une personne (enseignant ou élève)."""
+
+    aderess_list = []
+
     id: Optional[int] = field(default=None, init=False)
     street: str
     city: str
     postal_code: int
+
+    @classmethod
+    def create_adress(cls, street, city, postal_code):
+        new_adress = cls(street=street, city=city, postal_code=postal_code)
+        Adress.aderess_list.append(new_adress)
+        return new_adress
