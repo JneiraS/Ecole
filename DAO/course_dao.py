8e3424e87701c54f8737d8adfb05af4df546b253
NeiraJ <johnny.neira@gmail.com>
2024-08-27 10:54:06 +0200

    Simplification de la méthode read dans CourseManager pour améliorer la lisibilité et la maintenance. Ajout de la méthode update dans CourseManager pour permettre la mise à jour des informations d'un cours dans la base de données (à completer...).

---
+++ DAO/course_dao.py
@@ -0,0 +1,59 @@
+from DAO.database_manager import DatabaseConnectionManager
+from models.course import Course
+
+
+class CourseManager(DatabaseConnectionManager):
+    def create(self, new_course: Course):
+        """
+        Crée un nouveau cours dans la base de données.
+        :param new_course:
+        :return:
+        """
+        try:
+            self.open_connection()
+            query = (f"INSERT INTO courses (name, start_date, end_date) VALUES ('{new_course.name}',"
+                     f" '{new_course.start_date}', '{new_course.end_date}');")
+            self.cursor.execute(query)
+            self.conn.commit()
+            self.close_connection()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            self.close_connection()
+
+    def read(self, course_to_read: Course):
+        """
+        Renvoit le cours correspondant à l'entité dont l'id est id_course (ou None s'il n'a pu être trouvé)
+        :param course_to_read:
+        :return:
+        """
+        if course_to_read.id is not None:
+
+            try:
+                self.open_connection()
+                query = f"SELECT * FROM course where id_course = {course_to_read.id};"
+                self.cursor.execute(query)
+                rows = self.cursor.fetchall()
+                self.close_connection()
+                return rows
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                self.close_connection()
+
+        return None
+
+    def update(self, course_to_update: Course):
+        """Mett à jour la base de données avec les nouvelles informations du cours."""
+        self.open_connection()
+        query = (f" UPDATE course SET name = '{course_to_update.name}', start_date = '"
+                 f"{course_to_update.start_date}', end_date = '{course_to_update.end_date}', id_teacher = "
+                 f"{course_to_update.teacher};")
+        self.cursor.execute(query)
+        self.close_connection()
+
+    def delete(self): ...
+
+
+
+
+
