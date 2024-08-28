3ac6696986b98a7c829d960930db8a0f7c0c01b4
NeiraJ <johnny.neira@gmail.com>
2024-08-28 07:52:27 +0200

    Implémentation de la synchronisation des identifiants pour Course et Teacher
    - Introduction de l'interface SynchroniseId pour définir une méthode d'actualisation des identifiants
    - Implémentation de SynchroniseCourseId et SynchroniseTeacherId pour synchroniser les identifiants des instances Course et Teacher avec ceux de la base de données
    - Utilisation de DAO spécifiques pour chaque entité pour récupérer l'identifiant le plus récent après création

---
+++ business/synchronise_id.py
@@ -0,0 +1,35 @@
+import time
+from abc import ABC, abstractmethod
+from datetime import date
+
+from DAO.course_dao import CourseManager
+from DAO.teacher_dao import TeacherManager
+from models.course import Course
+from models.teacher import Teacher
+
+
+class SynchroniseId(ABC):
+    @abstractmethod
+    def actualise_id(self, class_name):
+        """
+        Met à jour l'id de la classe avec celui correrspondant à l'entité de la base de données
+        :param class_name:
+        :return:
+        """
+        pass
+
+
+class SynchroniseCourseId(SynchroniseId):
+
+    def actualise_id(self, course: Course):
+        course_manager = CourseManager()
+        last_course_id: int = course_manager.create(course)
+        course.id = last_course_id
+
+
+class SynchroniseTeacherId(SynchroniseId):
+
+    def actualise_id(self, teacher: Teacher):
+        teacher_manager = TeacherManager()
+        last_teacher_id: int = teacher_manager.create(teacher)
+        teacher.id = last_teacher_id
