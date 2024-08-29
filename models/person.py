8958850fb03f979730fc62639c62636fe7f63928
NeiraJ <johnny.neira@gmail.com>
2024-08-29 08:55:03 +0200

    Amélioration de la classe Person et ajout de méthodes de recherche
    - Modifié la méthode create_person pour ne plus prendre l'adresse en paramètre
    - Ajouté deux nouvelles méthodes statiques :
      - find_person_by_adress_id : recherche une personne par son adresse
      - find_person_by_id : recherche une personne par son identifiant
    - Amélioré la documentation des méthodes avec des docstrings plus détaillés
    - Optimisé la recherche en utilisant des générateurs pour les méthodes de recherche

--- models/person.py
+++ models/person.py
@@ -25,16 +25,32 @@ class Person(ABC):
         return f"{self.first_name} {self.last_name} ({self.age} ans)"
 
     @classmethod
-    def create_person(cls, first_name, last_name, age, address):
+    def create_person(cls, first_name, last_name, age):
         """
         Crée un nouvel objet Person et l'ajoute à la liste des personnes.
         :param first_name:
         :param last_name:
         :param age:
-        :param address:
+
         :return:
         """
         new_person = cls(first_name=first_name, last_name=last_name, age=age)
         Person.person_list.append(new_person)
         return new_person
 
+
+def find_person_by_adress_id(adress_id) -> Person:
+    """
+    Renvoit l'objet Person correspondant à l'id_number
+    :param adress_id:
+
+    """
+    return next((p for p in Person.person_list if p.address == adress_id), None)
+
+def find_person_by_id(id_number) -> Person:
+    """
+    Renvoit l'objet Person correspondant à l'id_number
+    :param id_number:
+
+    """
+    return next((p for p in Person.person_list if p.id == id_number), None)
