d6d84f70d043289c17bfcf459b34fd37362c468a
NeiraJ <johnny.neira@gmail.com>
2024-08-29 09:06:24 +0200

    Mise à jour de la structure de données et amélioration de l'affichage
    - Modification de la gestion des courses et étudiants :
      - Remplacement des imports directs par des imports via DAO
      - Ajout de méthodes de recherche dans les modèles (find_course_by_id, find_student_by_id)
    - Mise à jour de l'affichage :
      - Utilisation de la nouvelle méthode find_course_by_id dans DisplaySchoolCourses
      - Utilisation de la nouvelle méthode find_student_by_id dans DisplayStudents
      - Ajout d'un nouveau display pour les enseignants (DisplayTeachers)
    - Amélioration de l'interface utilisateur :
      - Ajout de fonctions de saisie interactive (modify_student, modify_cours)
      - Utilisation de la bibliothèque rich pour l'input utilisateur
    - Correction de la largeur des colonnes dans les tables d'affichage
    - Amélioration de la gestion des dates dans l'interface utilisateur

--- src/display.py
+++ src/display.py
@@ -1,13 +1,17 @@
 import os
 import platform
 from abc import abstractmethod, ABC
+from datetime import datetime
 
 from rich import box
 from rich.table import Table
 
+from DAO.course_dao import CourseManager
+from DAO.student_dao import StudentManager
 from business.school import School
-from models.course import Course
-from models.student import Student
+from models.course import Course, find_course_by_id
+from models.student import Student, find_student_by_id
+from models.teacher import Teacher
 
 
 class Display(ABC):
@@ -22,12 +26,10 @@ class Display(ABC):
 
 
 class DisplaySchoolCourses(Display):
-    def __init__(self):
-        self.school = School()
 
     def display(self):
         table = Table(show_header=True, title="Cours dispensés par l'Ecole",
-                      box=box.MINIMAL)
+                      box=box.SQUARE)
         table.add_column("ID", style="dim", width=12, justify="right")
         table.add_column("Nom", style="dim", width=12, justify="center")
 
@@ -43,17 +45,15 @@ class DisplaySchoolCourses(Display):
 
 
 class DisplayStudents(Display):
-    def __init__(self):
-        self.school = School()
 
     def display(self):
         table = Table(show_header=True, title="\nEtudiants presents dans l'Ecole",
-                      box=box.MINIMAL)
-        table.add_column("n° Etudiant", style="dim", width=12, justify="right")
-        table.add_column("Prenom", style="dim", width=12, justify="center")
+                      box=box.SQUARE, width=200)
+        table.add_column("n° Etudiant", style="dim", width=1, justify="right")
+        table.add_column("Prenom", style="dim", width=1, justify="center")
 
-        table.add_column("Nom", style="dim", width=12, justify="center")
-        table.add_column("Age", style="dim", width=12, justify="center")
+        table.add_column("Nom", style="dim", width=1, justify="center")
+        table.add_column("Age", style="dim", width=1, justify="center")
         table.add_column("Addresse", style="dim", width=12, justify="left")
         table.add_column("Cours suivis", style="dim", width=12, justify="left", min_width=400)
 
@@ -62,6 +62,35 @@ class DisplayStudents(Display):
                           str(student.address), str(student.courses_taken))
         # console.print(table)
         return table
+
+
+class DisplayTeachers(Display):
+    def display(self):
+        table = Table(show_header=True, title="\nEnseignants presents dans l'Ecole",
+                      box=box.SQUARE, width=200)
+        table.add_column("ID", style="dim", width=1, justify="right")
+        table.add_column("Prenom", style="dim", width=2, justify="center")
+
+        table.add_column("Nom", style="dim", width=12, justify="center")
+        table.add_column("Age", style="dim", width=12, justify="center")
+        table.add_column("Addresse", style="dim", width=12, justify="left")
+        table.add_column("Cours enseignés", style="dim", width=12, justify="left", no_wrap=True)
+
+        for teacher in Teacher.teacher_list:
+            table.add_row(str(teacher.id), str(teacher.first_name), str(teacher.last_name), str(teacher.age),
+                          str(teacher.address), str(teacher.courses_teached))
+        # console.print(table)
+        return table
+
+
+def rich_input(prompt):
+    """
+    Cette fonction affiche une boite de dialogue avec un texte de prompt et renvoie la saisie utilisateur.
+    Elle utilise la bibliothèque rich.
+    """
+    from rich.console import Console
+    console = Console()
+    return console.input(prompt)
 
 
 def clear_screen() -> None:
@@ -74,3 +103,39 @@ def clear_screen() -> None:
         os.system('cls')
     else:
         os.system('clear')
+
+
+def modify_student():
+    """
+    Permet de modifier un etudiant
+    :return:
+    """
+    sm = StudentManager()
+    response = rich_input('\n\t\tID de l\'Etudiant ? :')
+    student_to_modify = find_student_by_id(int(response))
+    print(f"\t\t{student_to_modify}")
+    student_to_modify.first_name = rich_input('\n\t\tNouveau Prenoms :')
+    student_to_modify.last_name = rich_input('\n\t\tNouveau Nom :')
+    student_to_modify.age = rich_input('\n\t\tNouveau Age :')
+    print(f"\t\t{student_to_modify}")
+    sm.update(student_to_modify)
+
+
+def modify_cours():
+    """
+    Permet de modifier un cours
+    :return:
+    """
+    cm = CourseManager()
+    response = rich_input('\n\t\tID du du Cours ? :')
+    course_to_modify: Course = find_course_by_id(int(response))
+    print(f"\t\t{course_to_modify}")
+    course_to_modify.name = rich_input('\n\t\tNouveau Nom du Cours :')
+    course_to_modify.start_date = (datetime.strptime
+                                   (rich_input('\n\t\tNouvelle Date de debut (''yyyy-mm-dd) :'),
+                                    '%Y-%m-%d').date())
+    course_to_modify.end_date = (datetime.strptime
+                                 (rich_input('\n\t\tNouvelle Date de fin (''yyyy-mm-dd) :'),
+                                  '%Y-%m-%d').date())
+    course_to_modify.teacher = int(rich_input('\n\t\tNouvel enseignant :'))
+    cm.update(course_to_modify)
