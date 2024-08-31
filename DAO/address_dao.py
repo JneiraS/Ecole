a46929da8d0e54f3fb5fae6da8630d4aef1139a7
NeiraJ <johnny.neira@gmail.com>
2024-08-31 21:12:22 +0200

    Amélioration de AddressManager : - Implémentation de la méthode `create` pour insérer de nouvelles adresses dans la base de données.

--- DAO/address_dao.py
+++ DAO/address_dao.py
@@ -17,7 +17,21 @@ class AddressManager(DatabaseConnectionManager):
             return []
 
     def create(self, class_name):
-        ...
+        try:
+            self.open_connection()
+            query = (
+                f"INSERT INTO {TableName.ADDRESS.value} (`street`, `city`, `postal_code`) "
+                f"VALUES ('{class_name.street}', '{class_name.city}', '{class_name.postal_code}');"
+            )
+            self.cursor.execute(query)
+            new_address_id = self.cursor.lastrowid
+            self.conn.commit()
+            self.close_connection()
+            return new_address_id
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            self.close_connection()
+            return []
 
     def read(self, table_name: str):
         ...
