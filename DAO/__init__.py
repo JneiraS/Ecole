9ec11a4b82ba53b4a628bb0fb7429a824c1f5e3b
NeiraJ <johnny.neira@gmail.com>
2024-08-31 21:11:21 +0200

    Add utility functions for database operations: - Implemented `execute_query` to execute SQL queries and return generated IDs. - Added `find_value` to fetch and return rows from database queries.

---
+++ DAO/__init__.py
@@ -0,0 +1,32 @@
+def execute_query(self, query: str) -> int:
+    """
+    Execute une requête dans la base de données
+    """
+    try:
+        self.open_connection()
+        self.cursor.execute(query)
+        id_generated = self.cursor.lastrowid
+        self.conn.commit()
+        self.close_connection()
+        return id_generated
+
+    except Exception as e:
+        print(f"Une erreur est survenue : {e}")
+        self.close_connection()
+        return None
+
+def find_value(self, query: str) -> int|None:
+    """
+       Execute une requête dans la base de données
+       """
+    try:
+        self.open_connection()
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        self.close_connection()
+        return rows
+
+    except Exception as e:
+        print(f"Une erreur est survenue : {e}")
+        self.close_connection()
+        return None
