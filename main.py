4f9ab9e61e3d443c873be88691d12e22ed65917f
NeiraJ <johnny.neira@gmail.com>
2024-08-31 21:09:38 +0200

    Refactor main.py: - Added new DAO and model imports for student and person management. - Enhanced `init` function with new sync operations for teachers. - Updated user prompts and logic for modifying students and courses. - Added debug prints for persons and students.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from rich.console import Console
-from rich.panel import Panel
+from DAO.student_dao import StudentDAO
+from models.person import Person
+from models.student import Student, find_student_by_id
 
 from src.display import DisplaySchoolCourses, clear_screen, DisplayStudents, DisplayTeachers, modify_student, \
     modify_cours, rich_input
 from src.factories import create_courses_from_query_results, creat_students_from_query_results, \
     create_teachers_from_query_results, create_persons_from_query_results, create_adresses_from_query_results
-from src.sync import sync_student_course_enrollment, sync_person_addess, sync_student_and_person_addess
+from src.sync import sync_student_course_enrollment, sync_person_addess, sync_student_and_person_addess, \
+    sync_teacher_and_person_addess, add_teacher_to_course
+from src.utils import student_creator
 
 
 def init():
     """
-    Initialise les donées de l'application
+    Initialise la base de données
     :return:
     """
     # chargement des elements de la base de données
@@ -22,14 +26,15 @@ def init():
     sync_student_course_enrollment()
     sync_person_addess()
     sync_student_and_person_addess()
+    sync_teacher_and_person_addess()
+    add_teacher_to_course()
 
 
 def main():
     while True:
         clear_screen()
-        console = Console()
-        console.print(Panel("--  ECOLE MANAGER  --", border_style="red"), justify="center")
 
+        console = Console()
         display_school_courses = DisplaySchoolCourses()
         display_students = DisplayStudents()
         display_teachers = DisplayTeachers()
@@ -39,16 +44,29 @@ def main():
 
         console.print(t1, t2, t3, justify='center')
 
-        response = rich_input('\n\t\tModifier un Etudiant ou un Cours ? :')
-        if 'Etudiant'.lower().find(response.lower()) != -1:
+        response = rich_input('\n\tModifier un Etudiant ou un Cours ? :')
+        if 'etudiant'.find(response.lower()) != -1:
             modify_student()
             continue
 
-        elif 'Cours'.lower().find(response.lower()) != -1:
+        elif 'cours'.find(response.lower()) != -1:
             modify_cours()
             continue
 
-if __name__ == "__main__":
 
+
+
+
+if __name__ == "__main__":
     init()
+    #student_creator(" 23 rue de la paix ", "Paris", "31000", "Calorine", "Jones", 11, 1)
+    print(Person.person_list)
+    print(Student.student_list)
+
+
+
+
+
     main()
+
+
