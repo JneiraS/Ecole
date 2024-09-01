9ebc866b7c33dced3371d47214fdc0be9ea7bfdd
NeiraJ <johnny.neira@gmail.com>
2024-09-01 14:15:51 +0200

    Refactorisation de StudentDAO pour améliorer la gestion des requêtes SQL : - Utilisation des fonctions `find_value` et `execute_query_with_condition` pour simplifier et sécuriser les opérations SQL. - Simplification de la méthode `create` pour l'insertion des étudiants. - Refactorisation des méthodes `create``get_all_student_ids`, `query_all`, `course_taken_by_student`, `update`, et `delete` en utilisant les nouvelles fonctions utilitaires. - Ajout de types explicites pour les paramètres des méthodes `assign_student_to_course` et `find_id_person_by_student_nbr`.

--- DAO/student_dao.py
+++ DAO/student_dao.py
@@ -1,4 +1,4 @@
-from DAO import execute_query, find_value
+from DAO import execute_query, find_value, execute_query_with_condition
 from DAO.database_manager import DatabaseConnectionManager
 from models.student import Student
 from src.table_name import TableName
@@ -6,132 +6,78 @@ from src.table_name import TableName
 
 class StudentDAO(DatabaseConnectionManager):
 
-    def create(self, person_id: int, student_nbr: int):
+    def create(self, person_id: int, student_nbr: int) -> int | None:
         """
         Crée un nouveau etudiant dans la base de données.
         :param student_nbr:
         :param person_id:
         :return:
         """
-        try:
-            self.open_connection()
-            query = (f"INSERT INTO {TableName.STUDENT.value} (`student_nbr`, `id_person`) VALUES ({student_nbr}, "
-                     f"{person_id}) ;")
+        query = (
+            f"INSERT INTO {TableName.STUDENT.value} (`student_nbr`, `id_person`) VALUES ({student_nbr}, "
+            f"{person_id}) ;")
 
-            self.cursor.execute(query)
-            new_student_id = self.cursor.lastrowid
-            self.conn.commit()
-            self.close_connection()
-            return new_student_id
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.close_connection()
+        return execute_query(self, query)
 
-
-    def get_all_student_ids(self):
+    def get_all_student_ids(self) -> int | None:
         """
         Renvoit la liste de tous id etudiants
         :return:
         """
-        try:
-            self.open_connection()
-            query = (
-                f"SELECT * FROM `{TableName.STUDENT.value}`;"
-            )
-            self.cursor.execute(query)
-            rows = self.cursor.fetchall()
-            self.close_connection()
-            return rows
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.close_connection()
-            return []
 
-    def query_all(self) -> list:
+        query = (
+            f"SELECT * FROM `{TableName.STUDENT.value}`;"
+        )
+
+        return find_value(self, query)
+
+    def query_all(self) -> list | None:
         """
         Renvoit la liste de tous les etudiants avec leurs informations
         :return:
         """
-        try:
-            self.open_connection()
-            query = (
-                f"SELECT DISTINCT  student.student_nbr, person.first_name, person.last_name, person.age "
-                f"FROM {TableName.STUDENT.value} "
-                f"JOIN person ON student.id_person = person.id_person "
-                f"JOIN takes ON student.student_nbr = takes.student_nbr;"
-            )
-            self.cursor.execute(query)
-            rows = self.cursor.fetchall()
-            self.close_connection()
-            return rows
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.close_connection()
-            return []
+        query = (
+            f"SELECT DISTINCT  student.student_nbr, person.first_name, person.last_name, person.age "
+            f"FROM {TableName.STUDENT.value} "
+            f"JOIN person ON student.id_person = person.id_person "
+            f"JOIN takes ON student.student_nbr = takes.student_nbr;"
+        )
+        return find_value(self, query)
 
-    def course_taken_by_student(self):
+    def course_taken_by_student(self) -> list | None:
         """
         Reenvoit l'information de la table takes
         :return:
         """
-        try:
-            self.open_connection()
-            query = f"SELECT * FROM `{TableName.TAKES.value}`;"
-            self.cursor.execute(query)
-            rows = self.cursor.fetchall()
-            self.close_connection()
-            return rows
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.close_connection()
-            return []
+        query = f"SELECT * FROM `{TableName.TAKES.value}`;"
+        return find_value(self, query)
 
     def update(self, student_to_update: Student) -> bool:
-        try:
 
-            self.open_connection()
-            query = (
-                f"UPDATE {TableName.PERSON.value} "
-                f"SET "
-                f"first_name = '{student_to_update.first_name}', "
-                f"last_name = '{student_to_update.last_name}', "
-                f"age = {student_to_update.age} "
-                f"WHERE "
-                f"id_person = {student_to_update.id};"
-            )
-            self.cursor.execute(query)
-            self.conn.commit()
-            self.close_connection()
-            return True
+        query = (
+            f"UPDATE {TableName.PERSON.value} "
+            f"SET "
+            f"first_name = '{student_to_update.first_name}', "
+            f"last_name = '{student_to_update.last_name}', "
+            f"age = {student_to_update.age} "
+            f"WHERE "
+            f"id_person = {student_to_update.id};"
+        )
+        return execute_query_with_condition(self, query)
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.close_connection()
-            return False
+    def delete(self, student_to_update: Student):
 
-    def delete(self,student_to_update: Student):
-        try:
-            self.open_connection()
-            query = f"DELETE FROM {TableName.STUDENT.value} WHERE id_person = {student_to_update.id};"
-            self.cursor.execute(query)
-            self.conn.commit()
-            self.close_connection()
-            return True
+        query = f"DELETE FROM {TableName.STUDENT.value} WHERE id_person = {student_to_update.id};"
+        return execute_query_with_condition(self, query)
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.close_connection()
-            return False
-
-    def assign_student_to_course(self, student_id, course_id):
+    def assign_student_to_course(self, student_id: int, course_id: int):
 
         query = f"""INSERT INTO `{TableName.TAKES.value}` (`student_nbr`, `id_course`) VALUES ({student_id},
         {course_id});"""
 
         return execute_query(self, query)
 
-    def find_id_person_by_student_nbr(self, student_nbr):
+    def find_id_person_by_student_nbr(self, student_nbr: int):
         query = f"SELECT id_person FROM `{TableName.STUDENT.value}` WHERE student_nbr = {student_nbr};"
 
         return find_value(self, query)
