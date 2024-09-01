54dde0ea9cf635c5fe25bb1faf4b01318c320f07
NeiraJ <johnny.neira@gmail.com>
2024-09-01 14:49:54 +0200

    Refactorisation et ajout de fonctionnalités dans `sync.py` : - Remplacement de `StudentManager` par `StudentDAO` et ajout de `TeacherDAO` pour la gestion des enseignants. - Ajout des fonctions `sync_teacher_and_person_addess` et `add_teacher_to_course` pour synchroniser l'adresse des enseignants avec celle des personnes et pour affecter les enseignants aux cours respectivement. - Ajout de blocs `try-except` pour gérer les exceptions lors de l'ajout de cours à un étudiant et lors de la synchronisation des adresses entre étudiants et personnes.

--- src/sync.py
+++ src/sync.py
@@ -1,8 +1,11 @@
-from DAO.student_dao import StudentManager
+from DAO.course_dao import CourseDAO
+from DAO.student_dao import StudentDAO
+from DAO.teacher_dao import TeacherDAO
 from models.adress import find_adress_by_id
 from models.course import find_course_by_id
 from models.person import Person, find_person_by_adress_id, find_person_by_id
 from models.student import find_student_by_id
+from models.teacher import find_teacher_by_id
 
 
 def sync_student_course_enrollment():
@@ -13,12 +16,15 @@ def sync_student_course_enrollment():
       par l'étudiant.
     :return:
     """
-    sm = StudentManager()
+    sm = StudentDAO()
     result = sm.course_taken_by_student()
     for i in result:
         student = find_student_by_id(i['student_nbr'])
         course = find_course_by_id(i['id_course'])
-        student.add_course(course)
+        try:
+            student.add_course(course)
+        except:
+            pass
 
 
 def sync_person_addess():
@@ -37,10 +43,42 @@ def sync_person_addess():
 
 
 def sync_student_and_person_addess():
-    sm = StudentManager()
+    sm = StudentDAO()
     result = sm.get_all_student_ids()
     for i in result:
-        person = find_person_by_id(i['id_person'])
-        student = find_student_by_id(i['student_nbr'])
 
-        student.address = person.address
+        person = find_person_by_id(i['id_person'])
+        print((i['student_nbr']))
+        student = find_student_by_id(i['student_nbr'])
+        print(student)
+
+        try:
+            student.address = person.address
+        except:
+            pass
+
+
+def sync_teacher_and_person_addess():
+    tm = TeacherDAO()
+    result = tm.get_all_teacher_ids()
+    for i in result:
+        person = find_person_by_id(i['id_person'])
+        teacher = find_teacher_by_id(i['id_teacher'])
+
+        teacher.address = person.address
+
+
+def add_teacher_to_course() -> None:
+    """
+    Affecte un enseignant à un cours sur la base des identifiants de cours.
+    :return:
+    """
+    cm = CourseDAO()
+    result = cm.get_all_course_ids()
+    for i in result:
+        teacher = find_teacher_by_id(i['id_teacher'])
+        course = find_course_by_id(i['id_course'])
+
+        course.teacher = teacher
+
+
